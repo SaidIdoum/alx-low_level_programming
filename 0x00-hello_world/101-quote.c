dcdd511aa7dbcac5112078ec4bd5bdd16f05cbe3
SaidIdoum <investtrav@gmail.com>
2023-06-18 11:52:24 -0700

    0

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,8 +3,8 @@
 
 int main(void)
 {
-	a=strlen("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
-	write(2,"and that piece of art is useful\" - Dora Korpar, 2015-10-19",a);
+	a = strlen("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19", a);
 
 	return (1);
 }
