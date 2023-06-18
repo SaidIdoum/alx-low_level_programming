4489b4b593b1f8a569bb4008969e2a055992161f
SaidIdoum <investtrav@gmail.com>
2023-06-18 11:55:48 -0700

    0

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,7 +6,7 @@
  */
 int main(void)
 {
-	a = strlen("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	int a = strlen("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
 	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19", a);
 
 	return (1);
