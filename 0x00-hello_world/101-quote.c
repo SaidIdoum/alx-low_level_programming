ed9f260e689badfa8b8e5ed3cce1d2895f54001d
SaidIdoum <investtrav@gmail.com>
2023-06-18 11:53:19 -0700

    0

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,6 +1,9 @@
 #include<unistd.h>
 #include<string.h>
-
+/* main - description
+ *
+ * Return: 1
+ */
 int main(void)
 {
 	a = strlen("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
