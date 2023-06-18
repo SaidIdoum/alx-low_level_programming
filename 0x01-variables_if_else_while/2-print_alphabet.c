4a3989eb6fe39b6b013fae002d5a088b00df5def
SaidIdoum <investtrav@gmail.com>
2023-06-18 14:16:13 -0700

    0

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main -  betty style doc for function main goes there
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int i = 97;
+
+	for (i < 123, i++)
+	{
+		putchar(i);
+		putchar(10);
+	}
+	return (0);
+}
+
+
