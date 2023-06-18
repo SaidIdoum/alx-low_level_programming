65c73bb647b07fae858c393cd55b797f1332a765
SaidIdoum <investtrav@gmail.com>
2023-06-18 12:00:38 -0700

    0

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,13 @@
 #include<unistd.h>
 #include<string.h>
-/* main - description
+/**
+ * main - description
  *
  * Return: 1
  */
 int main(void)
 {
-	int a = strlen("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", a);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 
 	return (1);
 }
