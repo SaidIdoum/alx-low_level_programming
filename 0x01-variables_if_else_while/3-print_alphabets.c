3e1ef5e9d862951eca888f351a2447bb2211fdc7
SaidIdoum <investtrav@gmail.com>
2023-06-19 10:01:01 -0700

    1

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -7,14 +7,15 @@
 int main(void)
 {
 	int i, j;
-	for(i=97; i<123; i++)
+
+	for (i = 97; i < 123; i++)
 	{
 		putchar(i);
 	}
-	for(j=65; j<91; j++)
+	for (j = 65; j < 91; j++)
 	{
 		putchar(j);
 	}
 	putchar(10);
-	return(0);
+	return (0);
 }
