3948ee2f129d66ef94a250d77be99f41c3990c23
SaidIdoum <investtrav@gmail.com>
2023-06-19 10:19:48 -0700

    1

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - description
+ *
+ * return: 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 97; i < 123; i++)
+	{
+		if (i != 101 && i != 113)
+			putchar(i);
+	}
+	return (0);
+}
