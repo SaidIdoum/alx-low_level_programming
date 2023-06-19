c4696aa961205e71910de2fedc75c48260b5f0aa
SaidIdoum <investtrav@gmail.com>
2023-06-19 10:56:46 -0700

    1

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main -  betty style doc for function main goes there
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 122; i < 96; i--)
+	{
+		putchar(i);
+	}
+	putchar(10);
+	return (0);
+}
+
+
