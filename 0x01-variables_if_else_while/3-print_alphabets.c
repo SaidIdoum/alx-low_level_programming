54cb0dcaf38ba4e57c4a08d027b1a3f13d14bd32
SaidIdoum <investtrav@gmail.com>
2023-06-19 09:58:33 -0700

    1

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main -  betty style doc for function main goes there
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int i, j;
+	for(i=97; i<123; i++)
+	{
+		putchar(i);
+	}
+	for(j=65; j<91; j++)
+	{
+		putchar(j);
+	}
+	putchar(10);
+	return(0);
+}
