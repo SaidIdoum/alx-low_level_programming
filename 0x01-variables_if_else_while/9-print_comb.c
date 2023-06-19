c79349e437495c0ec1384ce41195264e8e0ee751
SaidIdoum <investtrav@gmail.com>
2023-06-19 11:50:07 -0700

    1

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -8,12 +8,14 @@ int main(void)
 {
 	int i;
 
-	for (i = 48; i < 58; )
+	for (i = 48; i < 58; i++ )
 	{
 		putchar(i);
-		putchar(44);
-		putchar(32);
-		i = i+1;
+		if (i != 58)
+		{
+			putchar(44);
+			putchar(32);
+		}
 	}
 	putchar(10);
 	return (0);
