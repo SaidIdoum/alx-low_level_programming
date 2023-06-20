0162e62ed44c1ba6091a60272e98ca46315e4a98
SaidIdoum <investtrav@gmail.com>
2023-06-20 12:24:40 -0700

    2

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * jack_bauer - text
+ * Return: void
+ */
+void jack_bauer(void)
+{
+	int dh, h, dm, m;
+
+	for (dh = 0; dh <= 2 ; dh++)
+	{
+		for (h = 0; h <= 3; h++)
+		{
+			for (dm = 0 ; dm <= 5; dm++)
+			{
+				for (m = 0; m <= 9; m++)
+				{
+					_putchar(dh + '0');
+					_putchar(h + '0');
+					_putchar(':');
+					_putchar(dm + '0');
+					_putchar(m + '0');
+				}
+			}
+		}
+	}
+}
