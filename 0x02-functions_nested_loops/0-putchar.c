374d2d16ffc265a616f37e9a1d9f1a09c901a8f6
SaidIdoum <investtrav@gmail.com>
2023-06-20 09:30:28 -0700

    2

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <main.h>
+/**
+ * main -  betty style doc for function main goes there
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char word_to_print[] = "_putchar";
+	int i;
+
+	for (i = 0, i <= 7, i++)
+	{
+		_putchar(word_to_print[i]);
+	}
+	return (0);
+}
