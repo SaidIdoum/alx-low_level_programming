8f2d6b38dea5a0e33f8109a96ccceee350bed9f6
SaidIdoum <investtrav@gmail.com>
2023-06-20 15:50:17 -0700

    2

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -13,33 +13,27 @@ void print_times_table(int n)
 		for (r = 0; r <= n; r++)
 		{
 			for (c = 0; c <= n; c++)
-			{
-				prod = c * r;
+			{	prod = c * r;
 				if (c == 0)
-				{
-					_putchar('0');
-				}
+				{	_putchar('0'); }
 				else if (c > 0 && c <= n)
 				{
 					if (prod < 10)
-					{
-						_putchar(','); _putchar(' '); _putchar(' '); _putchar(' '); _putchar(prod + '0');
-					}
+					{	_putchar(',');
+						_putchar(' ');
+						_putchar(' ');
+						_putchar(' ');
+						_putchar(prod + '0'); }
 					else if (prod >= 10 && prod < 100)
-					{
-						_putchar(',');	_putchar(' '); _putchar(' '); _putchar((prod / 10) + '0'); _putchar((prod % 10) + '0');
-					}
+					{	_putchar(',');
+						_putchar(' ');
+						_putchar(' ');
+						_putchar((prod / 10) + '0');
+						_putchar((prod % 10) + '0'); }
 					else
-					{
-						_putchar(',');
+					{	_putchar(',');
 						_putchar(' ');
 						_putchar((prod / 100) + '0');
 						_putchar(((prod / 10) % 10) + '0');
 						_putchar(((prod % 100) % 10) + '0');
-					}
-				}
-			}
-			_putchar('\n');
-		}
-	}
-}
+					} } } _putchar('\n'); } } }
