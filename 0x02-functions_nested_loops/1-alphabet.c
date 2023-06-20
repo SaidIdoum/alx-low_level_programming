83be417f815d2878a20da4f3cc552becc71adc78
SaidIdoum <investtrav@gmail.com>
2023-06-20 10:54:43 -0700

    2

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -4,7 +4,7 @@
  *
  * Return: void
  */
-int main(void)
+void print_alphabet(void)
 {
 	char i;
 
@@ -13,6 +13,6 @@ int main(void)
 		_putchar(i);
 	}
 	_putchar('\n');
-	return (0);
+	return (void);
 }
 
