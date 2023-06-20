adaa7874aaa195e8c696e3e5d86a2e2a65b2a83f
SaidIdoum <investtrav@gmail.com>
2023-06-20 15:37:13 -0700

    2

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,54 @@
+#include "main.h"
+/**
+ * print_times_table - des
+ * @n: desc
+ * Return: void
+ */
+void print_times_table(int n);
+{
+	int r, c, prod;
+
+	if (n > 0 && n < 15)
+	{
+		for (r = 0; r <= n; r++)
+		{
+			for (c = 0; c <= n; c++)
+			{
+				prod = c * r;
+				if (c == 0)
+				{
+					_putchar('0');
+					_putchar(',');
+				}
+				else if (c > 0 && c <= n)
+				{
+					if (prod < 10)
+					{
+						_putchar(',');
+						_putchar(' ');
+						_putchar(' ');
+						_putchar(' ');
+						_putchar(prod + '0');
+					}
+					else if (prod >= 10 && prod < 100)
+					{
+						_putchar(',');
+						_putchar(' ');
+						_putchar(' ');
+						_putchar((prod / 10) + '0');
+						_putchar((prod % 10) + '0');
+					}
+					else
+					{
+						_putchar(',');
+						_putchar(' ');
+						_putchar((prod / 100) + '0');
+						_putchar(((prod / 10) % 10) + '0');
+						_putchar(((prod % 100) % 10) + '0');
+					}
+				}
+			}
+			_putchar('\n');
+		}
+	}
+}
