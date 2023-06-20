793fa994840c555bbee1e83a8f006d1d97c00809
SaidIdoum <investtrav@gmail.com>
2023-06-20 15:03:38 -0700

    2

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -14,14 +14,14 @@ void print_to_98(int n)
 	{
 		for (i = n; i < 98; i++)
 		{
-			printf("%i, ", i);
+			printf("%d, ", i);
 		}
 	}
 	else if (n > 98)
 		for (i = n; i > 98; i--)
 		{
-			printf("%i", i);
+			printf("%d", i);
 		}
-	printf("%n",n);
+	printf("%d",n);
 
 }
