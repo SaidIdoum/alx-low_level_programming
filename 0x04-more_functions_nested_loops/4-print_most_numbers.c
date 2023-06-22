40f749b695bb963b251f941124fda9966153504b
SaidIdoum <investtrav@gmail.com>
2023-06-22 05:37:04 -0700

    4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_most_numbers - prints numbers except two and four
+ * Return: void
+ */
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = 48; i < 57; i++)
+	{
+		if (i == 50 || i == 52)
+		{
+			i++;
+		}
+			_putchar(i + '0');
+	}
+	return (void);
+}
