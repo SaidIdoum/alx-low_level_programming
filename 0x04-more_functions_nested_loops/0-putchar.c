4d6287403c135c17c44997399288155b753e3008
SaidIdoum <investtrav@gmail.com>
2023-06-22 04:18:41 -0700

    4

---
+++ 0x04-more_functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * main -  betty style doc for function main goes there
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char word_to_print[] = "_putchar\n";
+	int i;
+
+	for (i = 0; i <= 8; i++)
+	{
+		_putchar(word_to_print[i]);
+	}
+	return (0);
+}
