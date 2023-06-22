c88f10b371ad88d8ddedb595ae8d3f7ae6b0831e
SaidIdoum <investtrav@gmail.com>
2023-06-22 14:39:53 -0700

    4

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_line - desc
+ * @n: parameter
+ * Return: void
+ */
+void print_line(int n)
+{
+	int i;
+
+	if (n > 0)
+	{
+		for (i = 1; i <= n; i++)
+		{
+			_putchar(95);
+		}
+	}
+	_putchar('\n');
+}
