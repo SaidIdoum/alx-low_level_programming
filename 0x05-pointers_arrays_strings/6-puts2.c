ed4261254d8524d02244b44b2a9544654b64b857
SaidIdoum <investtrav@gmail.com>
2023-06-28 14:35:33 -0700

    5

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,14 +1,17 @@
 #include "main.h"
 /**
  * puts2 - desc
- * @str:
+ * @str: desc
  */
 void puts2(char *str)
 {
+	int i;
+
+	i = 0;
 	while (str[i] != '\0')
 	{
-		_putchar(str[i];
-		_putchar('\n');
-		i = i+2
+		_putchar(str[i]);
+		i = i + 2;
 	}
+	_putchar('\n');
 }
