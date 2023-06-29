108eec1b39847b20f41e51b72f3fa8fbd69d19b3
SaidIdoum <investtrav@gmail.com>
2023-06-29 11:39:11 -0700

    6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -10,13 +10,37 @@ char *cap_string(char *string)
 
 	i = 0;
 	while (string[i] != '\0')
-	 {
-		 if (string[i] >= 'a' && string[i] <= 'z')
-		 {
-			 if (i == 0 || string[i - 1] == ' ' || string[i - 1] == '\t' || string[i - 1] == '\n' || string[i - 1] == ',' || string[i - 1] == ';' || string[i - 1] == '.' || string[i - 1] == '!' || string[i - 1] == '?' || string[i - 1] == '"' || string[i - 1] == '(' || string[i - 1] == ')' || string[i - 1] == '{' || string[i - 1] == '}')
-			 {
-				 string[i] = string[i] - 'a' + 'A';
-			 }
+	{
+		if (string[i] >= 'a' && string[i] <= 'z')
+		{
+			if (i == 0 || string[i - 1] == ' ' || string[i - 1] == '\t')
+			{
+				string[i] = string[i] - 'a' + 'A';
+			}
+			else if (string[i - 1] == '\n' || string[i - 1] == ',')
+			{
+				string[i] = string[i] - 'a' + 'A';
+			}
+			else if (string[i - 1] == '.' || string[i - 1] == '!')
+			{
+				string[i] = string[i] - 'a' + 'A';
+			}
+			else if (string[i - 1] == '"' || string[i - 1] == '(')
+			{
+				string[i] = string[i] - 'a' + 'A';
+			}
+			else if (string[i - 1] == '{' || string[i - 1] == '}')
+			{
+				string[i] = string[i] - 'a' + 'A';
+			}
+			else if (string[i - 1] == ';' || string[i - 1] == '?')
+			{
+				string[i] = string[i] - 'a' + 'A';
+			}
+			else if (string[i - 1] == ')')
+			{
+				string[i] = string[i] - 'a' + 'A';
+			}
 		}
 		i++;
 	}
