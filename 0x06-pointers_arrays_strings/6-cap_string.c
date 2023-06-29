39abdd8163fac0070136fd56efd715056ae5e524
SaidIdoum <investtrav@gmail.com>
2023-06-29 11:04:55 -0700

    6

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * cap_string - desc
+ * @string: desc
+ * Return: desc
+ */
+char *cap_string(char *string)
+{
+	int i;
+
+	i = 0;
+	while (string[i] != '\0')
+	 {
+		 if (string[i] >= 'a' && string[i] <= 'z')
+		 {
+			 if (string[i - 1] == ' ' || string[i - 1] == '\t' || string[i - 1] == '\n' || string[i - 1] == ',' || string[i - 1] == ';' || string[i - 1] == '.' || string[i - 1] == '!' || string[i - 1] == '?' || string[i - 1] == '"' || string[i - 1] == '(' || string[i - 1] == ')' || string[i - 1] == '{' || string[i - 1] == '}')
+			 {
+				 string[i] = string[i] - 'a' + 'A';
+			 }
+		}
+		i++;
+	}
+		return (string);
+}
