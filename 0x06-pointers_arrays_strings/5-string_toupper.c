1a1a25182e07fdefd5e7cf19d855bd4d5fe57868
SaidIdoum <investtrav@gmail.com>
2023-06-29 07:28:02 -0700

    6

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * string_toupper - desc
+ * @string: desc
+ * Return: desc
+ */
+char *string_toupper(char *string)
+{
+	int i;
+
+	i = 0;
+	while(string[i] != '\0')
+	{
+		if (string[i] >= 'a' && string[i] <= 'z')
+		{
+			string[i] = string[i] - 'a' + 'A';
+		}
+		i++;
+	}
+	return (string);
+}
