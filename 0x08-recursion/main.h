b74b07e145beb8bf67e3bca31e9d3a026bac3958
SaidIdoum <investtrav@gmail.com>
2023-07-05 09:28:12 -0700

    8

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -5,4 +5,4 @@ void _print_rev_recursion(char *s);
 int factorial(int n);
 int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
-_sqrt_guess(int n, int guess);
+int _sqrt_guess(int n, int guess);
