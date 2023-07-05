fd812a1c208facdfb85d016eba9c6bfa36d94f74
SaidIdoum <investtrav@gmail.com>
2023-07-05 09:25:21 -0700

    8

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,37 @@
+#include "main.h"
+/**
+ * _sqrt_recursion - desc
+ * @n: par
+ * Return: int
+ */
+int _sqrt_recursion(int n)
+{
+	int guess, result;
+
+	guess = (n / 2) + 1;
+	result = _sqrt_guess(n, guess);
+	if (n < 0)
+	{
+		return (-1);
+	}
+	else if (n <= 1)
+	{
+		return (n);
+	}
+	return (result);
+}
+int _sqrt_guess(int n, int guess)
+{
+	if (guess * guess == n)
+	{
+		return (guess);
+	}
+	else if (guess * guess > n)
+	{
+		return (-1);
+	}
+	else
+	{
+		return (_sqrt_guess(n, guess + 1));
+	}
+}
