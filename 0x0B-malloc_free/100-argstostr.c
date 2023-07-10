9eddad035c95c3106fdd0d5805ecfe115dd7dbb5
SaidIdoum <investtrav@gmail.com>
2023-07-10 10:38:22 -0700

    b

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,47 @@
+#include "main.h"
+/**
+ * argstostr - desc
+ * @ac: par
+ * @av: par
+ * Return:
+ */
+char *argstostr(int ac, char **av)
+{
+	int i, j, size, full_size;
+	char *str;
+
+	if (ac == 0 || av == NULL)
+	{
+		return (NULL);
+	}
+	full_size = 0;
+	for (i = 0; i < ac; i++)
+	{
+		size = 0;
+		while (av[i][size] != '\0')
+		{
+			size++;
+		}
+		full_size += size + 1;
+	}
+	str = malloc(sizeof(char) * (full_size + 1));
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	j = 0;
+	for (i = 0; i < ac; i++)
+	{
+		size = 0;
+		while (av[i][size] != '\0')
+		{
+			str[j] = av[i][size];
+			j++;
+			size++;
+		}
+		str[j] = '\n';
+		j++;
+	}
+	str[j] = '\0';	
+	return (str);
+}
