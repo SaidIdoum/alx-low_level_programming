fca26883afc865bc9249af3b7bd4c05ca9eab2fa
SaidIdoum <investtrav@gmail.com>
2023-07-10 10:25:01 -0700

    b

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,20 @@
+#include "main."
+/**
+ * free_grid - desc
+ * @grid: par
+ * @height: par
+ */
+void free_grid(int **grid, int height)
+{
+	int i;
+
+	if (grid == NULL || height <= 0)
+	{
+		return;
+	}
+	for (i = 0; i < height; i++)
+	{
+		free(grid[i]);
+	}
+	free(grid);
+}
