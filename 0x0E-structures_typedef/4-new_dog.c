2518551f4ad0c454f86c2e466a0ce200963bd470
SaidIdoum <investtrav@gmail.com>
2023-07-21 06:09:14 -0700

    e

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,44 +1,40 @@
 #include "dog.h"
 #include <stdlib.h>
 #include <string.h>
+
 /**
- * new_dog - ffffff 
- * @name: fffff
- * @age: fffff
- * @owner: ffffff
- * Return: fffffffffff
+ * new_dog - desc
+ * @name: fgsdf
+ * @age: sdfgsdfgsdf
+ * @owner: sdfgsdfgdfg
+ * Return: sdfgsdfgdfg
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t *new_dog;
+	dog_t *new_dog = malloc(sizeof(dog_t));
+	char *name_copy, *owner_copy;
 
-	if (name == NULL || owner == NULL)
-	{
-		return (NULL);
-	}
-
-	new_dog = malloc(sizeof(dog_t));
 	if (new_dog == NULL)
-	{
 		return (NULL);
-	}
 
-	new_dog->name = strdup(name);
-	if (new_dog->name == NULL)
+	name_copy = strdup(name);
+	if (name_copy == NULL)
 	{
 		free(new_dog);
 		return (NULL);
 	}
 
+	owner_copy = strdup(owner);
+	if (owner_copy == NULL)
+	{
+		free(name_copy);
+		free(new_dog);
+		return (NULL);
+	}
+
+	new_dog->name = name_copy;
 	new_dog->age = age;
-
-	new_dog->owner = strdup(owner);
-	if (new_dog->owner == NULL)
-	{
-		free(new_dog->name);
-		free(new_dog);
-		return (NULL);
-	}
+	new_dog->owner = owner_copy;
 
 	return (new_dog);
 }
