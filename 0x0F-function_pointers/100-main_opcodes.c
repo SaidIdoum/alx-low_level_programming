13cc29098054eeedd05a8adc7e836c6dd9b14358
SaidIdoum <investtrav@gmail.com>
2023-07-23 02:47:00 -0700

    f

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - desc
+ * @argc: par
+ * @argv: par
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+    int num_bytes, i;
+    unsigned char *main_func = (unsigned char *)main;
+
+    if (argc != 2)
+    {
+        printf("Error\n");
+        exit(1);
+    }
+
+    num_bytes = atoi(argv[1]);
+    if (num_bytes < 0)
+    {
+        printf("Error\n");
+        exit(2);
+    }
+
+    for (i = 0; i < num_bytes; i++)
+    {
+        printf("%02x", main_func[i]);
+        if (i < num_bytes - 1)
+            printf(" ");
+    }
+    printf("\n");
+
+    return (0);
+}
