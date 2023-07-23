c05b0a90f9439850dc0f38458a998cb00c7cfa32
SaidIdoum <investtrav@gmail.com>
2023-07-23 02:05:32 -0700

    f

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -8,16 +8,17 @@
  */
 int int_index(int *array, int size, int (*cmp)(int))
 {
-    int i;
+	int i;
 
-    if (array != NULL && cmp != NULL && size > 0)
-    {
-        for (i = 0; i < size; i++)
-        {
-            if (cmp(array[i]) != 0)
-                return (i);
-        }
-    }
-    return (-1);
+	if (array != NULL && cmp != NULL && size > 0)
+	{
+		for (i = 0; i < size; i++)
+		{
+			if (cmp(array[i]) != 0)
+			{
+				return (i);
+			}
+		}
+	}
+	return (-1);
 }
-
