4350232d1b4a6337351c57458caa6f7c917a0341
SaidIdoum <investtrav@gmail.com>
2023-07-23 02:55:23 -0700

    f

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -8,29 +8,28 @@
  */
 int main(int argc, char *argv[])
 {
-    int num_bytes, i;
-    unsigned char *main_func = (unsigned char *)main;
+	int num_bytes, i;
+	unsigned char *main_func = (unsigned char *)main;
 
-    if (argc != 2)
-    {
-        printf("Error\n");
-        exit(1);
-    }
-
-    num_bytes = atoi(argv[1]);
-    if (num_bytes < 0)
-    {
-        printf("Error\n");
-        exit(2);
-    }
-
-    for (i = 0; i < num_bytes; i++)
-    {
-        printf("%02x", main_func[i]);
-        if (i < num_bytes - 1)
-            printf(" ");
-    }
-    printf("\n");
-
-    return (0);
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+	num_bytes = atoi(argv[1]);
+	if (num_bytes < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+	for (i = 0; i < num_bytes; i++)
+	{
+		printf("%02x", main_func[i]);
+		if (i < num_bytes - 1)
+		{
+			printf(" ");
+		}
+	}
+	printf("\n");
+	return (0);
 }
